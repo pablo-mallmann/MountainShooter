1508092a1be1f7051d614e196762600696047d27
Pablo Mallmann <mallmannspablo@gmail.com>
2025-06-10 22:44:33 -0300

    Alterações aula 1

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('Hello World')
-print('Teste')
+from idlelib.run import quitting
+
+import pygame
+
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+
+while True:
+    # Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit() # Close Window
+            quit() # End Pygame
